57c6d441f5b8c6d65f15da8829eb51052bfb60f9
jenny-m-16 <jenny.mak@kellogg.northwestern.edu>
2023-02-17 03:40:35 +0000

    wow it finally works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+        @post["place_id"] = params["place_id"]
     end
     
     def create
@@ -17,7 +18,8 @@ class PostsController < ApplicationController
         @post["title"] = params["post"]["title"]
         @post["description"] = params["post"]["description"]
         @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places"
     end
 end
